d2394135324289d1ddd2c72c050067657e70a9c3
daniil0518 <dsvecenovskij@gmai.com>
2024-05-28 21:39:41 +0200

    update contact form

--- app/contact/page.js
+++ app/contact/page.js
@@ -31,7 +31,7 @@ const Index = () => {
     
     const { NEXT_PUBLIC_EMAILJS_USER_ID, NEXT_PUBLIC_EMAILJS_SERVICE_ID, NEXT_PUBLIC_EMAILJS_TEMPLATE_ID } = process.env;
     
-    emailjs.send(
+    emailjs.sendForm(
       NEXT_PUBLIC_EMAILJS_SERVICE_ID,
       NEXT_PUBLIC_EMAILJS_TEMPLATE_ID,
       formData,
--- components/Contact.js
+++ components/Contact.js
@@ -42,7 +42,7 @@ const ContactForm = () => {
     
     const { NEXT_PUBLIC_EMAILJS_USER_ID, NEXT_PUBLIC_EMAILJS_SERVICE_ID, NEXT_PUBLIC_EMAILJS_TEMPLATE_ID } = process.env;
     
-    emailjs.send(
+    emailjs.sendForm(
       NEXT_PUBLIC_EMAILJS_SERVICE_ID,
       NEXT_PUBLIC_EMAILJS_TEMPLATE_ID,
       formData,
